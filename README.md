d8ee1a8cde543cca3465c5dc4ebf5040e1220c0c
pii-terr <70038733+pii-terr@users.noreply.github.com>
2022-05-15 23:58:14 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+p1 es el hito 1
+p2.2 es el hito 3 con la pagina chilena
+p4 es el hito 3 con la pagina extranjera 
+hito4par1 es el hito 4 para chile
+hito4par1,1 es el hito 4 para españa
+hito4parte2 es el hito 4 para chile/ funciona igual para españa
---
+++ p2,2.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+
+
+url = "https://www.pizzahut.cl/usuario/inicio"
+
+
+driver = webdriver.Chrome("/Users/Pedro/Downloads/chromedriver_win32 (1)/chromedriver.exe")
+driver = webdriver.Chrome("/Users/Pedro/Downloads/chromedriver_win32 (1)/chromedriver.exe")
+driver.maximize_window()
+driver.get("https://yopmail.com/es/email-generator")
+email=driver.find_element(By.ID,"egen").text
+passs=email.split('@')
+driver.get(url)
+driver.get(url)
+driver.find_element(By.XPATH ,'//*[@id="modHeadLogin"]/a[2]').click()
+driver.find_element(By.XPATH ,'//*[@id="Email"]').send_keys(email)
+driver.find_element(By.XPATH ,'//*[@id="FirstPhone"]').send_keys("911111111")
+driver.find_element(By.XPATH ,'//*[@id="Password"]').send_keys("11111111")
+driver.find_element(By.XPATH ,'//*[@id="ConfirmPassword"]').send_keys("11111111")
+
+driver.find_element(By.XPATH ,'//*[@id="userRegister"]/div[5]/span[1]').click()
+driver.find_element(By.XPATH ,'//*[@id="lbAcceptEmails"]').click()
+driver.find_element(By.XPATH ,'//*[@id="lbAcceptSMS"]').click()
+driver.find_element(By.XPATH ,'//*[@id="btRegister"]/span').click()
+
+driver.find_element(By.XPATH ,'//*[@id="headerLinks"]/ul/li[2]/a').click()
+driver.find_element(By.XPATH ,'/html/body/div[3]/ul/li[2]').click()
+driver.find_element(By.XPATH ,'//*[@id="my_account"]/div/div[2]/ul/li[2]').click()
+
+#[@id="my_account"]/div/div[2]/ul/li[2]
+
+driver.find_element(By.XPATH ,'//*[@id="user_new_password"]').send_keys("111111112")
+driver.find_element(By.XPATH ,'//*[@id="user_confirm_password"]').send_keys("111111112")
+driver.find_element(By.XPATH ,'//*[@id="changePassword"]/div/div/div').click()
+driver.find_element(By.XPATH ,'//*[@id="passConfirmPopup"]').send_keys("11111111")
+#//*[@id="popup"]/div[3]/p[2]/a/span
+driver.find_element(By.XPATH ,'//*[@id="popup"]/div[3]/p[2]/a/span').click()
+
+bd=open("usuarios_chile.txt","a")
+bd.write(email+'\n')
+bd.close()
+
+import time
+while True:
+    print("This prints once a minute.")
+    time.sleep(60) # Delay for 1 minute (60 seconds).
+
+
